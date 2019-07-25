eeee6b148df49dfbea3e3c3ae60d48f9adb26353
garameki <garameki@users.noreply.github.com>
2019-07-25 18:09:12 +0900

    ノードをマウスで動かす

---
+++ plate.js
@@ -0,0 +1,65 @@
+
+/****************************************************************
+ * 使い方
+ * <script type="application/javascript"></script>
+ * <script type="application/javascript">
+ * onload = function() {
+ * 	var element = myPlate.new()
+ * 	document.getElementsByTagName('body')[0].appendChild(element);
+ * 	element.innerText = 'example';
+ * };
+ * </script>
+ *
+ * 効果
+ * マウスによるドラッグができるようになる
+ ******************************************************************/
+(function() {
+let gElement = void 0;
+let gXDown,gYDown;
+let gYMove,gXMove;
+myPlate = { };
+
+Object.defineProperty(myPlate,'new',{value:fNew,configurable:false});
+
+function fNew() {
+	const element = document.createElement('div');
+	element.style.position = 'relative';
+	element.style.left = '0px';
+	element.style.top = '0px';
+	element.addEventListener('mousedown',(event)=>{
+		gXDown = event.clientX;
+		gYDown = event.clientY;
+	
+		if(gElement == void 0) {
+			gElement = event.target;
+
+			gLeft = Number(gElement.style.left.match(/[-]?\d+(?:\.\d+)?/)[0]);
+			gTop = Number(gElement.style.top.match(/[-]?\d+(?:\.\d+)?/)[0]);
+
+
+		}
+print2(gXDown,gYDown);
+		
+	},false);
+	element.addEventListener('mouseup',(event)=>{
+		gElement = void 0;
+
+	},false);
+	return element;
+};
+/*gXMove,gYMoveはglobalではないので、()();の中に置きます*/
+window.addEventListener('mousemove',(event)=>{
+	gXMove = event.clientX;
+	gYMove = event.clientY;
+
+	print3(gXMove,gYMove);
+	if(gElement != void 0) {
+		gElement.style.left = (gLeft + gXMove - gXDown ).toString() + 'px';
+		gElement.style.top = (gTop + gYMove - gYDown ).toString() + 'px';
+	}
+	event.preventDefault();
+
+},false);
+})();
+
+
